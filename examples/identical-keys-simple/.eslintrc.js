d488f5b974c2fbb35726cbbc5ac4e7e699159c47
Andrew MacCuaig <andrewmaccuaig@gmail.com>
2023-05-02 21:53:31 -0400

    bump deps

--- examples/identical-keys-simple/.eslintrc.js
+++ examples/identical-keys-simple/.eslintrc.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   root: true, // since this example folder is embedded into the project. just ignore this.
-  extends: ["plugin:@maccuaa/eslint-plugin-i18n-json/recommended"],
+  extends: ["plugin:@maccuaa/i18n-json/recommended"],
   rules: {
     // option for this rule the absolute path to the comparision file the plugin should require.
     "@maccuaa/i18n-json/identical-keys": [
